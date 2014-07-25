0dfaf92c346cdbaab2fa6ba786d14367112f6fb4
Will Price <willprice94+dev@gmail.com>
2014-07-25 15:19:44 +0100

    Add manage.py

---
+++ manage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+from flask.ext.script import Manager
+import subprocess
+
+import mentor_finder
+NOSE_ARGS = ['--exe', '--with-coverage', '--cover-package=mentor_finder']
+
+manager = Manager(mentor_finder.app)
+
+@manager.command
+def run():
+    mentor_finder.main()
+
+@manager.command
+def test():
+   subprocess.call(["nosetests"] + NOSE_ARGS)
+
+
+if __name__ == "__main__":
+    manager.run()
